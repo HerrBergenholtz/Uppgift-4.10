c45dc4757f646565673e4d1aed94a5a9574506c6
HerrBergenholtz <zbdv8800@gmail.com>
2023-10-27 00:35:15 +0200

    Add project files.

---
+++ Uppgift 4.10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Uppgift_4._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många \"X\" vill du ha per grupp?");
+            int xNr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många \"O\" per grupp?");
+            int oNr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många \"O\" mönster per rad?");
+            int oMönsterNr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många rader?");
+            int rader = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rader; i++)
+            {
+                for (int j = 0; j < oMönsterNr + 1; j++)
+                {
+                    for (int n = 0; n < xNr; n++)
+                    {
+                        Console.Write("X");
+                    }
+
+                    if (j < oMönsterNr)
+                    {
+                        Console.Write("-");
+                        for (int l = 0; l < oNr; l++)
+                        {
+                            Console.Write("O");
+                        }
+                        Console.Write("-");
+                    }
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
